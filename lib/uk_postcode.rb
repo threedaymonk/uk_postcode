b1bf3a2766bdc40fa96520cd8c4d92512f5f7890
Paul Battley <pbattley@gmail.com>
2010-01-06 11:05:25 +0000

    Added rdoc.

--- lib/uk_postcode.rb
+++ lib/uk_postcode.rb
@@ -6,30 +6,45 @@ class UKPostcode
 
   attr_reader :raw
 
-  def initialize(raw)
-    @raw = raw.upcase
+  # Initialise a new UKPostcode instance from the given postcode string
+  #
+  def initialize(postcode_as_string)
+    @raw = postcode_as_string.upcase
   end
 
+  # Returns true if the postcode is a valid full postcode (e.g. W1A 1AA) or outcode (e.g. W1A)
+  #
   def valid?
     raw.match(RE_FULL) || raw.match(RE_OUTCODE_ONLY)
   end
 
+  # Returns true if the postcode is a valid full postcode (e.g. W1A 1AA)
+  #
   def full?
     raw.match(RE_FULL)
   end
 
+  # Returns true if the postcode is a valid outcode (e.g. W1A)
+  #
   def outcode?
     !full? && raw.match(RE_OUTCODE_ONLY)
   end
 
+  # The left-hand part of the postcode, e.g. W1A 1AA -> W1A
+  #
   def outcode
     raw[RE_FULL, 1] || raw[RE_OUTCODE_ONLY]
   end
 
+  # The right-hand part of the postcode, e.g. W1A 1AA -> 1AA
+  #
   def incode
     raw[RE_FULL, 2]
   end
 
+  # Render the postcode as a normalised string, i.e. in upper case and with spacing.
+  # Returns an empty string if the postcode is not valid.
+  #
   def to_str
     [outcode, incode].compact.join(" ")
   end
