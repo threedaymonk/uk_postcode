af2ce0c862ee48473f3e3c5d06a808aa5218fe20
Paul Battley <pbattley@gmail.com>
2015-02-06 18:25:56 +0000

    Test OI/01 errors exhaustively

--- spec/full_postcode_spec.rb
+++ spec/full_postcode_spec.rb
@@ -13,21 +13,46 @@ describe "Full set of postcodes" do
     'M83000003' => :isle_of_man
   }
 
-  it "parses and finds the country of every extant postcode" do
+  def each_postcode
     skip "Skipping because SKIP_FULL_TEST was set" if ENV['SKIP_FULL_TEST']
     skip "Skipping because #{CSV_PATH} does not exist" unless File.exist?(CSV_PATH)
 
     CSV.foreach(CSV_PATH, headers: [:postcode, :country]) do |row|
+      country = COUNTRIES.fetch(row[:country])
       outcode = row[:postcode][0, 4].strip
       incode  = row[:postcode][4, 3].strip
-      country = COUNTRIES.fetch(row[:country])
+      yield outcode, incode, country
+    end
+  end
 
+  it "parses and finds the country of every extant postcode" do
+    each_postcode do |outcode, incode, country|
       postcode = UKPostcode.parse(outcode + incode)
 
       expect(postcode).not_to be_nil
       expect(postcode.outcode).to eq(outcode)
       expect(postcode.incode).to eq(incode)
       expect(postcode.country).to eq(country)
+    end
+  end
+
+  it "parses postcodes with letters in place of numbers" do
+    each_postcode do |outcode, incode, _country|
+      postcode = UKPostcode.parse((outcode + incode).tr("01", "OI"))
+
+      expect(postcode).not_to be_nil
+      expect(postcode.outcode).to eq(outcode)
+      expect(postcode.incode).to eq(incode)
+    end
+  end
+
+  it "parses postcodes with numbers in place of letters" do
+    each_postcode do |outcode, incode, _country|
+      postcode = UKPostcode.parse((outcode + incode).tr("OI", "01"))
+
+      expect(postcode).not_to be_nil
+      expect(postcode.outcode).to eq(outcode)
+      expect(postcode.incode).to eq(incode)
     end
   end
 end
