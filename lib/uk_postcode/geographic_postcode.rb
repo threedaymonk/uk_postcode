974e61ebf134e61189d988e077e1fa6add9fbcaf
rikas <oterosantos@gmail.com>
2018-11-29 16:32:31 +0000

    Avoid returning nil when one of the parts is nil

--- lib/uk_postcode/geographic_postcode.rb
+++ lib/uk_postcode/geographic_postcode.rb
@@ -65,7 +65,7 @@ module UKPostcode
     # Returns true if the postcode is a valid full postcode (e.g. W1A 1AA)
     #
     def full?
-      area && district && sector && unit && true
+      !(area.nil? || district.nil? || sector.nil? || unit.nil?)
     end
 
     # Any geographic postcode is assumed to be valid
